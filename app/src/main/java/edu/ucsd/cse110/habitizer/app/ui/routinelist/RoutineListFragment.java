168b5f16287b9adb18465663e4eb5efc8622405c
Ferretelic <kyleshouki@gmail.com>
2025-02-08 22:40:46 -0800

    added placeholder routine list fragment

---
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/routinelist/RoutineListFragment.java
@@ -0,0 +1,49 @@
+package edu.ucsd.cse110.habitizer.app.ui.routinelist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ucsd.cse110.habitizer.app.MainViewModel;
+
+import edu.ucsd.cse110.habitizer.app.databinding.FragmentRoutineListBinding;
+
+public class RoutineListFragment extends Fragment {
+    private MainViewModel activityModel;
+    private FragmentRoutineListBinding view;
+
+    public RoutineListFragment() {}
+
+    public static RoutineListFragment newInstance() {
+        RoutineListFragment fragment = new RoutineListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        var modelOwner = requireActivity();
+        var modelFactory = ViewModelProvider.Factory.from(MainViewModel.initializer);
+        var modelProvider = new ViewModelProvider(modelOwner, modelFactory);
+        this.activityModel = modelProvider.get(MainViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.view = FragmentRoutineListBinding.inflate(inflater, container, false);
+        return view.getRoot();
+    }
+}

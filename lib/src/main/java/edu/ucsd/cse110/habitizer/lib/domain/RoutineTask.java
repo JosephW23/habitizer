baec3b63d89b9f9a381309d40ebffdf0c3d5b91b
Ferretelic <kyleshouki@gmail.com>
2025-02-08 22:34:15 -0800

    added id in RoutineTask

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineTask.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineTask.java
@@ -1,26 +1,30 @@
 package edu.ucsd.cse110.habitizer.lib.domain;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.Objects;
 
 public class RoutineTask {
+    private @Nullable Integer id;
     private @NonNull String title;
     private int priority;
     private boolean isChecked;
 
-    public RoutineTask(@NonNull String title, int priority, boolean isChecked) {
+    public RoutineTask(@Nullable Integer id, @NonNull String title, int priority, boolean isChecked) {
         if (title == null || title.trim().isEmpty()) {
             throw new IllegalArgumentException("RoutineTask title cannot be blank");
         }
         if (priority <= 0) {
             throw new IllegalArgumentException("RoutineTask priority must be an integer greater than 0");
         }
+        this.id = id;
         this.title = title;
         this.priority = priority;
         this.isChecked = isChecked;
     }
 
+    public @Nullable Integer id() { return id; }
     public @NonNull String title() { return title; }
 
     public int priority() { return priority; }

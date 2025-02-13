34622a63f3bea8df6e6d96847e2b2e60437b387f
Ferretelic <kyleshouki@gmail.com>
2025-02-13 15:10:51 -0800

    show menu bar

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/MainActivity.java
@@ -1,8 +1,15 @@
 package edu.ucsd.cse110.habitizer.app;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import edu.ucsd.cse110.habitizer.app.databinding.ActivityMainBinding;
+import edu.ucsd.cse110.habitizer.app.ui.routinelist.RoutineListFragment;
+import edu.ucsd.cse110.habitizer.app.ui.tasklist.TaskListFragment;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +19,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Title");
+        Log.d("Menu", "Create Main");
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("Menu", "Create Menu");
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        var itemId = item.getItemId();
+
+        if (itemId == R.id.action_bar_menu_swap_views) {
+//            swapFragments();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

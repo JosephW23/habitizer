0c63c4765972a5e090ae334eb1383b0bda9f9854
Ferretelic <kyleshouki@gmail.com>
2025-02-13 11:14:04 -0800

    remove swap fragments

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/MainActivity.java
@@ -5,12 +5,8 @@ import android.os.Bundle;
 import edu.ucsd.cse110.habitizer.app.databinding.ActivityMainBinding;
 import androidx.appcompat.app.AppCompatActivity;
 
-import edu.ucsd.cse110.habitizer.app.ui.tasklist.TaskListFragment;
-import edu.ucsd.cse110.habitizer.app.ui.routinelist.RoutineListFragment;
-
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding view;
-    private boolean isShowingRoutine = true;
     private MainViewModel model;
 
     @Override
